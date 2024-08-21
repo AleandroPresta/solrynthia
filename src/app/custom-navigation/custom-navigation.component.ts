32885df9dbed06c2b7d201c9168b48d5084e2471
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2024-08-21 11:05:52 +0200

    Completed navbar

--- src/app/custom-navigation/custom-navigation.component.ts
+++ src/app/custom-navigation/custom-navigation.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { AccountIconComponent } from '../account-icon/account-icon.component';
 
 @Component({
   selector: 'app-custom-navigation',
   standalone: true,
-  imports: [],
+  imports: [
+    AccountIconComponent
+  ],
   templateUrl: './custom-navigation.component.html',
   styleUrl: './custom-navigation.component.css'
 })
