a7a59c6eddd0c152f101a206356c9c08d4f163de
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2024-08-21 14:50:02 +0200

    Added Home and About routings to footer

--- src/app/custom-footer/custom-footer.component.ts
+++ src/app/custom-footer/custom-footer.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-custom-footer',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './custom-footer.component.html',
   styleUrl: './custom-footer.component.css'
 })
