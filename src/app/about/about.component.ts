ba87c31265226ab91f9bd0ea8c64cb7360555621
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2024-08-21 15:29:28 +0200

    Basic about section

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CustomNavigationComponent } from "../custom-navigation/custom-navigation.component";
 import { CustomFooterComponent } from '../custom-footer/custom-footer.component';
 import { RouterOutlet } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [RouterOutlet, CustomNavigationComponent, CustomFooterComponent],
+  imports: [RouterOutlet, CustomNavigationComponent, CustomFooterComponent, FontAwesomeModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
