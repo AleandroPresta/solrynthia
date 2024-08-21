b8682278b873cb27754afc50832f5d8209da39ce
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2024-08-21 15:31:37 +0200

    refactor: Remove unnecessary FontAwesomeModule import and update navigation component

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -2,12 +2,11 @@ import { Component } from '@angular/core';
 import { CustomNavigationComponent } from "../custom-navigation/custom-navigation.component";
 import { CustomFooterComponent } from '../custom-footer/custom-footer.component';
 import { RouterOutlet } from '@angular/router';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [RouterOutlet, CustomNavigationComponent, CustomFooterComponent, FontAwesomeModule],
+  imports: [RouterOutlet, CustomNavigationComponent, CustomFooterComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
