992edcf906951acdc7df23cafcfc8db7647a4cdd
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2024-08-21 11:44:33 +0200

    Presentation completed both desktop and mobile

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CustomNavigationComponent } from "../custom-navigation/custom-navigation.component";
+import { PresentationComponent } from './presentation/presentation.component';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
   imports: [
     CustomNavigationComponent,
+    PresentationComponent
   ],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css'
