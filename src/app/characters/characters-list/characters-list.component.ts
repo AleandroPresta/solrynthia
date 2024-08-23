3d818f7f1096e3e60b6b9c8fa0b4eceae8991841
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2024-08-23 09:45:18 +0200

    Removed some footer pages that I will not implement

--- src/app/characters/characters-list/characters-list.component.ts
+++ src/app/characters/characters-list/characters-list.component.ts
@@ -20,7 +20,7 @@ export class CharactersListComponent {
   @Input() charactersList: Character[] = [
     new Character(
       'The Cursed Mage',
-      'Description',
+      'A powerful sorcerer doomed by dark magic, battling both enemies and his own inner demons.',
       '',
       'https://raw.githubusercontent.com/AleandroPresta/static-api/master/api/v1/solrynthria/characters/cursed_mage/cover.jpeg',
       '',
@@ -28,11 +28,27 @@ export class CharactersListComponent {
     ),
     new Character(
       'The Lost Priest',
-      'Description',
+      'A wandering cleric seeking redemption, haunted by the echoes of a forgotten faith.',
       '',
       'https://raw.githubusercontent.com/AleandroPresta/static-api/master/api/v1/solrynthria/characters/lost_priest/cover.jpeg',
       '',
       'https://raw.githubusercontent.com/AleandroPresta/static-api/master/api/v1/solrynthria/characters/lost_priest/character.png'
+    ),
+    new Character(
+      'The Shadow Huntress',
+      'A stealthy assassin who moves like a whisper in the dark, wielding twin daggers with deadly precision.',
+      '',
+      'https://raw.githubusercontent.com/AleandroPresta/static-api/master/api/v1/solrynthria/characters/shadow_huntress/cover.jpg',
+      '',
+      'https://raw.githubusercontent.com/AleandroPresta/static-api/master/api/v1/solrynthria/characters/shadow_huntress/character.png'
+    ),
+    new Character(
+      'The Flameborn Warrior',
+      'A fierce warrior infused with the essence of fire, her very presence radiating heat and power.',
+      '',
+      'https://raw.githubusercontent.com/AleandroPresta/static-api/master/api/v1/solrynthria/characters/flameborn_worrior/cover.jpg',
+      '',
+      'https://raw.githubusercontent.com/AleandroPresta/static-api/master/api/v1/solrynthria/characters/flameborn_worrior/character.png'
     ),
   ];
 }
