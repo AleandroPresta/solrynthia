868aada1706fffc775ef62f73131cebba7228a35
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2024-08-23 10:59:22 +0200

    Completed landscape component

--- src/app/landscapes/Landscape.ts
+++ src/app/landscapes/Landscape.ts
@@ -1,9 +1,11 @@
 export class Landscape {
     name: string;
     pageUrl: string;
+    imgUrl: string;
 
-    constructor(name: string, pageUrl: string) {
+    constructor(name: string, pageUrl: string, imgUrl: string) {
         this.name = name;
         this.pageUrl = pageUrl;
+        this.imgUrl = imgUrl;
     }
 }
--- src/app/landscapes/landscapes-list/landscapes-list.component.ts
+++ src/app/landscapes/landscapes-list/landscapes-list.component.ts
@@ -1,18 +1,22 @@
 import { Component } from '@angular/core';
 import { LandscapeCardComponent } from "./landscape-card/landscape-card.component";
 import { Landscape } from '../Landscape';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-landscapes-list',
   standalone: true,
-  imports: [LandscapeCardComponent],
+  imports: [LandscapeCardComponent, NgFor],
   templateUrl: './landscapes-list.component.html',
   styleUrl: './landscapes-list.component.css'
 })
 export class LandscapesListComponent {
 
   landscapes: Landscape[] = [
-
+    new Landscape('Lake of Lumara', '', 'https://img.freepik.com/premium-photo/imaginary-landscapes_796443-992.jpg'),
+    new Landscape('The Mystical Tower', '', 'https://e0.pxfuel.com/wallpapers/323/212/desktop-wallpaper-gray-building-illustration-ancient-babylon-toi-fantasy.jpg'),
+    new Landscape('The Angel Statue', '', 'https://cdn.pixabay.com/photo/2022/06/03/13/40/fantasy-7240172_1280.jpg'),
+    new Landscape('Thalindra\'s Cascade', '', 'https://blenderartists.org/uploads/default/original/4X/d/9/b/d9b2401e33c08300e2cffd0443a66273d5cccf2e.jpeg'),
   ];
 
 }
