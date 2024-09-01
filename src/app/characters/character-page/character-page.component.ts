3fe7d07bacd0d4ac0c828a57102cd1fa7a6c6a8f
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2024-09-01 08:57:09 +0200

    Added info about characters in characterpage

--- src/app/characters/character-page/character-page.component.ts
+++ src/app/characters/character-page/character-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Character } from '../Character';
 
 @Component({
   selector: 'app-character-page',
@@ -11,6 +12,46 @@ import { ActivatedRoute } from '@angular/router';
 export class CharacterPageComponent implements OnInit {
 
   slug!: string;
+  character!: Character;
+
+  charactersList: { [key: string]: Character } = {
+    "cursed-mage": new Character(
+      "The Cursed Mage",
+      "cursed-mage",
+      "A powerful sorcerer doomed by dark magic, battling both enemies and his own inner demons.",
+      "",
+      "https://raw.githubusercontent.com/AleandroPresta/static-api/master/api/v1/solrynthria/characters/cursed_mage/cover.jpeg",
+      "",
+      "https://raw.githubusercontent.com/AleandroPresta/static-api/master/api/v1/solrynthria/characters/cursed_mage/character.png"
+    ),
+    "lost-priest": new Character(
+      "The Lost Priest",
+      "lost-priest",
+      "A wandering cleric seeking redemption, haunted by the echoes of a forgotten faith.",
+      "",
+      "https://raw.githubusercontent.com/AleandroPresta/static-api/master/api/v1/solrynthria/characters/lost_priest/cover.jpeg",
+      "",
+      "https://raw.githubusercontent.com/AleandroPresta/static-api/master/api/v1/solrynthria/characters/lost_priest/character.png"
+    ),
+    "shadow-huntress": new Character(
+      "The Shadow Huntress",
+      "shadow-huntress",
+      "A stealthy assassin who moves like a whisper in the dark, wielding twin daggers with deadly precision.",
+      "",
+      "https://raw.githubusercontent.com/AleandroPresta/static-api/master/api/v1/solrynthria/characters/shadow_huntress/cover.jpg",
+      "",
+      "https://raw.githubusercontent.com/AleandroPresta/static-api/master/api/v1/solrynthria/characters/shadow_huntress/character.png"
+    ),
+    "flameborn-warrior": new Character(
+      "The Flameborn Warrior",
+      "flameborn-warrior",
+      "A fierce warrior infused with the essence of fire, her very presence radiating heat and power.",
+      "",
+      "https://raw.githubusercontent.com/AleandroPresta/static-api/master/api/v1/solrynthria/characters/flameborn_worrior/cover.jpg",
+      "",
+      "https://raw.githubusercontent.com/AleandroPresta/static-api/master/api/v1/solrynthria/characters/flameborn_worrior/character.png"
+    )
+  };
 
   constructor(private router: ActivatedRoute) { }
 
@@ -20,6 +61,9 @@ export class CharacterPageComponent implements OnInit {
         this.slug = params['slug'];
       }
     )
+
+    this.character = this.charactersList[this.slug];
+    console.log(this.character);
   }
 
 }
