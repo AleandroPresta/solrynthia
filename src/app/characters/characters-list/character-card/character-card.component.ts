4c52497060fef67d409b2980e0561766810bac2c
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2024-09-01 08:59:00 +0200

    Added character slug and link to the card's image

--- src/app/characters/characters-list/character-card/character-card.component.ts
+++ src/app/characters/characters-list/character-card/character-card.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-character-card',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './character-card.component.html',
   styleUrl: './character-card.component.css'
 })
@@ -13,5 +14,6 @@ export class CharacterCardComponent {
   @Input() coverImageUrl!: string;
   @Input() titleImageUrl!: string;
   @Input() cardImageUrl!: string;
+  @Input() slug!: string;
 
 }
